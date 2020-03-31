024a8e5d8e842ce219158b516e3273729e261d7e
surbhi-jain-30-99 <44546216+surbhi-jain-30-99@users.noreply.github.com>
2020-03-31 21:31:45 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,7 @@
+#import array as arr
+
+#arr.array()
+
+from array import *
+val = array('i', [5,9,8,4,2],[2,4,5,7])
+print (val)
---
+++ python.py
@@ -0,0 +1,28 @@
+Python
+Python is a programming language that is simple and easy to understand.
+It is used in web development, machine learning, statistical processing, and more.
+
+
+quotation
+
+String:
+In Python, a sequence of characters, like "Hello Python", is called a string.
+A string needs to be enclosed in single ' or double " quotes.
+The output will be the same either way.
+If you don't put it in quotes, you will get an error.
+
+Adding  comments:
+Adding # at the beginning will make the entire line a comment.
+Comments will not be executed when running code, so you can use them to leave notes.
+
+Integers:
+You can use numbers like integers in programming.
+Unlike strings, they don't need to be enclosed in quotes.
+You can add and subtract integers just like you do in math.
+The spaces before and after operators are not required, but they will make the code easier to read.
+
+
+Difference betweenStrings and Integers:
+
+Strings and integers are interpreted differently in programming.
+Like the images below, 3 + 5 will print 8, which is the result of the addition. However, if you enclose it in quotes and make it a string, the output will stay as 3 + 5.
